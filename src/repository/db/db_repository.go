6d56acd915ec7e74b2a312a370c6b794d35a80bf
luizmoitinho <luiz.moitinho@olxbr.com>
2023-02-11 14:25:15 -0300

    feat: creating a cassandra session at repository

--- src/repository/db/db_repository.go
+++ src/repository/db/db_repository.go
@@ -1,6 +1,9 @@
 package db
 
 import (
+	"log"
+
+	"github.com/luizmoitinho/bookstore_oauth_api/src/clients/cassandra"
 	"github.com/luizmoitinho/bookstore_oauth_api/src/domain/access_token"
 	"github.com/luizmoitinho/bookstore_oauth_api/src/utils/errors"
 )
@@ -17,5 +20,11 @@ type dbRespository struct {
 }
 
 func (db *dbRespository) GetByID(string) (*access_token.AcessToken, *errors.RestError) {
+	//TODO: implement get access token from CassandraDB
+	session, err := cassandra.GetSession()
+	if err != nil {
+		log.Println(err)
+	}
+	defer session.Close()
 	return nil, errors.NewInternalServerError("database connection not implemented yet.")
 }
