0607b3089e0006116bc2478fb0c6e0df3cea61d6
luizmoitinho <luiz.moitinho@olxbr.com>
2023-02-11 15:03:16 -0300

    feat: set env port at cassandra db

--- src/clients/cassandra/cassandra.go
+++ src/clients/cassandra/cassandra.go
@@ -1,7 +1,9 @@
 package cassandra
 
 import (
+	"log"
 	"os"
+	"strconv"
 
 	"github.com/gocql/gocql"
 )
@@ -16,6 +18,13 @@ func InitCluster() {
 	cluster.Authenticator = gocql.PasswordAuthenticator{
 		Username: os.Getenv("CASSANDRA_USERNAME"),
 		Password: os.Getenv("CASSANDRA_PASSWORD"),
+	}
+
+	if port, err := strconv.ParseInt(os.Getenv("CASSANDRA_PORT"), 10, 64); err != nil {
+		cluster.Port = 9042
+		log.Printf("cannot convert CASSANDRA_PORT at .env: %v", err)
+	} else {
+		cluster.Port = int(port)
 	}
 
 	cluster.Keyspace = os.Getenv("CASSANDRA_KEY_SPACE")
