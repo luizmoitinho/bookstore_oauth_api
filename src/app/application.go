62ecdd0b64f7d75b14ec564c4a7600908d304914
luizmoitinho <luiz.moitinho@olxbr.com>
2023-02-11 14:26:13 -0300

    feat: create the first cassandra connection to guarantee the access by api

--- src/app/application.go
+++ src/app/application.go
@@ -1,7 +1,11 @@
 package app
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/luizmoitinho/bookstore_oauth_api/src/clients/cassandra"
 	"github.com/luizmoitinho/bookstore_oauth_api/src/domain/access_token"
 	"github.com/luizmoitinho/bookstore_oauth_api/src/http"
 	"github.com/luizmoitinho/bookstore_oauth_api/src/repository/db"
@@ -12,8 +16,14 @@ var (
 )
 
 func StartApplication() {
+	session, dbErr := cassandra.GetSession()
+	if dbErr != nil {
+		panic(dbErr)
+	}
+	session.Close()
+
 	accessTokenHandler := http.NewHandler(access_token.NewService(db.New()))
 
 	router.GET("/oauth/access_token/:access_token_id", accessTokenHandler.GetByID)
-	router.Run(":8080")
+	router.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
 }
